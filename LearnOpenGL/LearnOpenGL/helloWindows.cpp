35d414f135cc387814adec7e74042b79a450b466
王啸川 <m15048619182@163.com>
2018-07-31 00:36:50 +0800

    << update of new draw

--- LearnOpenGL/LearnOpenGL/helloWindows.cpp
+++ LearnOpenGL/LearnOpenGL/helloWindows.cpp
@@ -90,29 +90,45 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     
-    float vertices[] = {
-        -0.5f,-0.5f,0.0f,
-        0.5f,-0.5f,0.0f,
-        0.0f,0.5f,0.0f
-    };  // 顶点数据
+    float firstTriangle[] = {
+        -0.9f,-0.5f,0.0f,
+        -0.45f,0.5f,0.0f,
+        0.0f,-0.5f,0.0f
+    };
     
-    unsigned int VBO,VAO;   // 顶点缓冲对象
-    glGenVertexArrays(1,&VAO);  // 生成对应ID的VAO对象
-    glGenBuffers(1,&VBO);   // 生成对应ID的VBO对象
-    glBindVertexArray(VAO); // 绑定VAO对象
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);  // 将VBO对象绑定到GL_ARRAY_BUFFER上，任何该目标的缓冲都会配置当前绑定的缓冲（VBO）。
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW); // 将定义的顶点数据复制到缓冲的内存中
+    float secondTriangle[] =
+    {
+        0.0f,-0.5f,0.0f,
+        0.45f,0.5f,0.0f,
+        0.9f,-0.5f,0.0f
+    };
+    
+    unsigned int VBOs[2],VAOs[2];   // 顶点缓冲对象
+    glGenVertexArrays(2,VAOs);  // 生成对应ID的VAO对象
+    glGenBuffers(2,VBOs);   // 生成对应ID的VBO对象
+    glBindVertexArray(VAOs[0]); // 绑定VAO对象
+    
+    glBindBuffer(GL_ARRAY_BUFFER,VBOs[0]);  // 将VBO对象绑定到GL_ARRAY_BUFFER上，任何该目标的缓冲都会配置当前绑定的缓冲（VBO）。
+    glBufferData(GL_ARRAY_BUFFER,sizeof(firstTriangle),firstTriangle,GL_STATIC_DRAW); // 将定义的顶点数据复制到缓冲的内存中
     
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);  // 设置顶点属性指针
     glEnableVertexAttribArray(0);   // 启用与0索引相关联的顶点数组
-    
     glBindBuffer(GL_ARRAY_BUFFER,0);
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens.
     // 你可以在之后取消绑定这个VAO，这样做其他的VAO调用将不会意外的修改这个VAO，尽管它很少发生。
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     // 修改其他VAO需要调用glBindVertexArray，所以我们通常不会在没有直接需要的情况下取消绑定VAO（也不是VBO）。
-    glBindVertexArray(0);
     
+    glBindVertexArray(VAOs[1]);
+    glBindBuffer(GL_ARRAY_BUFFER,VBOs[1]);
+    
+    glBufferData(GL_ARRAY_BUFFER,sizeof(secondTriangle),secondTriangle,GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+    
+    
+    glBindVertexArray(0);
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);   // 设置线条绘制
     while(!glfwWindowShouldClose(window))
     {// Render Loop 渲染循环
         processInput(window);
@@ -121,13 +137,16 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);   // 状态使用函数
         
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+        glBindVertexArray(VAOs[0]);
         glDrawArrays(GL_TRIANGLES,0,3);
-        
+        //glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);   // 绘制图案类型，顶点数，索引类型，偏移量
+        glBindVertexArray(VAOs[1]);
+        glDrawArrays(GL_TRIANGLES,0,3);
         glfwPollEvents();   // 检查有无触发事件
         glfwSwapBuffers(window);    //交换颜色缓冲
     }
-    
+    glDeleteBuffers(2,VBOs);
+    glDeleteBuffers(2,VAOs);
     glfwTerminate(); // 清理资源并释放应用程序
     //glViewport(0,0,800,600);  用来设置视口维度
     return 0;
