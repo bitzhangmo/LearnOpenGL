12015272f29ec8301f9b1a2d4d3fc2f858915250
王啸川 <m15048619182@163.com>
2019-12-10 22:02:36 +0800

    no message

--- Chapter/02/02_06.cpp
+++ Chapter/02/02_06.cpp
@@ -190,6 +190,7 @@ int main(void)
         lightingShader.setVec3("light.position",camera.Position);
         lightingShader.setVec3("light.direction",camera.Front);
         lightingShader.setFloat("light.cutOff",glm::cos(glm::radians(12.5f)));  // radians把角度转换为弧度
+        lightingShader.setFloat("light.outerCutOff",glm::cos(glm::radians(17.5f)));
         lightingShader.setVec3("viewPos",camera.Position);
 
         lightingShader.setVec3("light.ambient",0.2f,0.2f,0.2f);
--- Shader/0206/LightFragment.glsl
+++ Shader/0206/LightFragment.glsl
@@ -36,11 +36,11 @@ uniform vec3 viewPos;
 
 void main() {
 
-    vec3 lightDir = normalize(light.position - FragPos);
-    float theta =  dot(lightDir,normalize(-light.direction));
+    //vec3 lightDir = normalize(light.position - FragPos);
 
-    if(theta > light.cutOff)
-    {
+
+//    if(theta > light.cutOff)
+  //  {
         // 环境光
         vec3 ambient = light.ambient * vec3(texture(material.diffuse,TexCoords));
 
@@ -58,19 +58,26 @@ void main() {
         float spec = pow(max(dot(viewDir,reflectDir),0.0),material.shininess);
         vec3 specular = vec3(texture(material.specular,TexCoords)) * spec * light.specular;
 
+    float theta =  dot(lightDir,normalize(-light.direction));
+    float epsilon = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff)/epsilon,0.0,1.0);
+    diffuse *= intensity;
+    specular *= intensity;
+
         float distance = length(light.position - FragPos);
         float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
 
-        //ambient *= attenuation;
+        ambient *= attenuation;
         diffuse *= attenuation;
         specular *= attenuation;
 
         vec3 result = (ambient + diffuse + specular);
         FragColor = vec4(result,1.0);
-    }
-    else
-    {
-        FragColor = vec4(light.ambient * vec3(texture(material.specular,TexCoords)),1.0);
-    }
+  //  }
+    //else
+    //{
+    //    FragColor = vec4(light.ambient * vec3(texture(material.specular,TexCoords)),1.0);
+    //}
+
 
 }
